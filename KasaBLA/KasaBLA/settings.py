addc244803c1510a417dbc63705f9099d82d3dd5
Christelle.Yemeck <christelle.yemeck@fda.hhs.gov>
2021-05-05 09:26:35 -0400

    Version 3 executable file may052021

--- KasaBLA/KasaBLA/settings.py
+++ KasaBLA/KasaBLA/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'KasaBLA.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '//cbsfs03/OTRR/databasetest/sampledb.sqlite3',
     }
 }
 
